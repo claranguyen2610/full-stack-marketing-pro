d7c0b81d1a9736d3bdcd6c8b32dfaa2094d3f72e
clara <clara@gmail.com>
2025-06-10 22:19:16 +0700

    Connect to Google Analytics

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -7,6 +7,16 @@ const ScrollToTop = () => {
   useEffect(
     () => {
       window.scrollTo(0, 0); // Scrolls to the top of the page
+
+      // Send Google Analytics 4 Page View event
+      if (window.gtag) {
+        // Check if gtag is loaded
+        window.gtag('event', 'page_view', {
+          page_path: location.pathname + location.search, // Includes query parameters
+          page_location: window.location.href, // Full URL
+          page_title: document.title // Current document title
+        });
+      }
     },
     [pathname]
   );
